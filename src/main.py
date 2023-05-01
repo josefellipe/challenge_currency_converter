73c39be918b40de3d1db2217021fc08295ea34fe
C99479A <jose.serqueira@br.experian.com>
2023-04-30 23:12:15 -0300

    Create main.py and config basic for fastapi

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return{'message': 'ok'}
+
+if __name__ == "__main__":
+    uvicorn.run(app)
